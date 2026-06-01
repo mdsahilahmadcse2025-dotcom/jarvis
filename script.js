1f7e323c429826e9eebe9aac21900d3b8739f62e
mdsahilahmadcse2025-dotcom <mdsahilahmad.cse2025@nsec.ac.in>
2026-06-01 17:12:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ if (!SpeechRecognition) {
 // REST Call to Gemini
 async function askAI(prompt) {
     try {
-        const url = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+        const url = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash-8b:generateContent?key=${apiKey}`;
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
